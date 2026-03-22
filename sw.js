6529df8d2c9ec4c2034fc825a92cc2a36f5e8ccb
Nova Pax <novapax@proton.me>
2026-03-22 07:24:50 -0400

    Initial commit

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'cat-ticket-v1';
+const FILES = [
+  '/',
+  '/index.html',
+  '/CAT_Logo.jpg',
+  '/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
